c59fb21f84c21dff94b8f1f594d64afc0c354988
DevReaxul <hreazul11@gmail.com>
2023-05-05 16:59:24 +0600

    final commit

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const chef = require('./data/chef.json')
 const chefRecipe = require('./data/recipe.json')
 
 app.use(cors())
-app.get('/chef-recipe', (req, res) => { 
+app.get('/chef-recipe', (req, res) => {
     res.send(chefRecipe)
-})
-app.get('/chef-recipe/:id', (req, res) => { 
+});
+app.get('/chef-recipe/:id', (req, res) => {
     const id = req.params.id;
     const selectedChefs = chefRecipe.find(chef => chef.id == id)
     res.send(selectedChefs)
-})
+});
 app.get('/chef', (req, res) => {
     res.send(chef)
-})
+});
 app.get('/', (req, res) => {
     res.send('Server side running...')
-})
+});
 app.listen(port, () => {
     console.log(`Chef's delight listening on port ${port}`)
-})
+});
